80b02dba19367ed5fe55139287baedf901313ef6
FJV VASCO <gustavo.berto@aluno.senac.rio>
2025-11-06 18:36:06 -0300

    Refactor option selection logic in ex12.py

--- ex12.py
+++ ex12.py
@@ -1,7 +1,8 @@
 print ("Selecione a opção")
 print ("1 - par ou ímpar.")
 print ("2 - maior ou menor.")
-print ("3 - veja o dobro.")op = int(input("Digite o número: "))
+print ("3 - veja o dobro.")
+op = int(input("Digite o número: "))
 match op:
     case 1:
         num = int(input("Escolha um número: "))
@@ -24,4 +25,5 @@ match op:
     print(f"Opção {a} é {dobro}")  
     case _:
     print("Opção inválida")
+
 
